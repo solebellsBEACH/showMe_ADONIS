086df5daf7eb6d946589dd8191d93695bcdf88b0
Lucas Xavier <lucassxxavier@gmail.com>
2023-04-19 16:39:26 -0300

    feat: adding more documents

--- mock/seedData/documents.ts
+++ mock/seedData/documents.ts
@@ -13,24 +13,54 @@ type DocumentsSeedDataType = {
   is_personal_bio?: boolean
   image_url: string
 }
-
+const perfilImage =
+  'https://raw.githubusercontent.com/solebellsBEACH/showMeAssets/main/assets/perfilImage.jpg'
+const documentModel = {
+  page_id: PageUuids.home,
+  header: 'Documento para testes em português',
+  primary_text: 'Documento para testes em português',
+  secondary_text: 'Documento para testes em português',
+  image_url: perfilImage,
+}
 export const DocumentsSeedData: DocumentsSeedDataType[] = [
   {
+    ...documentModel,
     id: v4(),
-    page_id: PageUuids.home,
-    header: 'Documento para testes em português',
-    primary_text: 'Documento para testes em português',
-    secondary_text: 'Documento para testes em português',
     language: LanguageCodeEnum.english,
-    image_url: 'Documento para testes em português',
   },
   {
+    ...documentModel,
     id: v4(),
-    page_id: PageUuids.home,
-    header: 'Documento para testes em inglês',
-    primary_text: 'Documento para testes em inglês',
-    secondary_text: 'Documento para testes em inglês',
+    language: LanguageCodeEnum.portuguese,
+  },
+  {
+    ...documentModel,
+    id: v4(),
     language: LanguageCodeEnum.english,
-    image_url: 'Documento para testes em inglês',
+  },
+  {
+    ...documentModel,
+    id: v4(),
+    language: LanguageCodeEnum.portuguese,
+  },
+  {
+    ...documentModel,
+    id: v4(),
+    language: LanguageCodeEnum.english,
+  },
+  {
+    ...documentModel,
+    id: v4(),
+    language: LanguageCodeEnum.portuguese,
+  },
+  {
+    ...documentModel,
+    id: v4(),
+    language: LanguageCodeEnum.english,
+  },
+  {
+    ...documentModel,
+    id: v4(),
+    language: LanguageCodeEnum.portuguese,
   },
 ]
